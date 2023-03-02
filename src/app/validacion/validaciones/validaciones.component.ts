4131939fb7c59d6db6ef96d0167e003669ffccdc
pablo moncada <pablo.moncada@softtek.com>
2023-03-02 10:23:05 -0300

    Segunda actividad mejoras

--- src/app/validacion/validaciones/validaciones.component.ts
+++ src/app/validacion/validaciones/validaciones.component.ts
@@ -11,13 +11,14 @@ export class ValidacionesComponent implements OnInit {
 
   form: FormGroup;
   errorAge: string[] = [];
+  errorName: string[] = [];
+  errorLastName: string[] = [];
 
   constructor( private formBuilder: FormBuilder ) {
     this.form = this.formBuilder.group({
         name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
         lastname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
         age: new FormControl('', [Validators.required, Validators.min(18), Validators.max(65)])
-  
     });
    }
 
@@ -25,16 +26,50 @@ export class ValidacionesComponent implements OnInit {
 
     this.form.valueChanges.subscribe(dt => {
       this.errorAge = this.validarEdad();
+      this.errorName = this.validarNombre();
+      this.errorLastName = this.validarApellido();
     })
      
    }
-   
-  get f(){
-    return this.form.controls;
-  }
   
-  submit(){
+   guardar(){
     console.log(this.form.value);
+  }
+
+  validarNombre(): string[] {
+    const control = this.form.get('name')?.errors;
+    const error: string[] = [];
+    if (control?.['required']) {
+
+      error.push('Name is required.');
+    }
+    if (control?.['minlength']) {
+
+      error.push('Name should be 3 character.');
+    }
+    if (control?.['maxlength']) {
+
+      error.push('Name must not exceed 20 character.');
+    }
+    return error;
+  }
+
+  validarApellido(): string[] {
+    const control = this.form.get('lastname')?.errors;
+    const error: string[] = [];
+    if (control?.['required']) {
+
+      error.push('LastName is required.');
+    }
+    if (control?.['minlength']) {
+
+      error.push('LastName should be 3 character.');
+    }
+    if (control?.['maxlength']) {
+
+      error.push('LastName must not exceed 20 character.');
+    }
+    return error;
   }
 
   validarEdad(): string[] {
